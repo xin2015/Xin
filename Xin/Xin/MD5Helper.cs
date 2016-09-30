07b1b25dc695b498599bbb0a446fe57ed3f6bf25
liuhx <564030525@qq.com>
2016-09-30 13:51:01 +0800

    MD5

---
+++ Xin/Xin/MD5Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xin
+{
+    public class MD5Helper
+    {
+        public static string Encrypt(string text)
+        {
+            MD5 md5 = MD5.Create();
+            return Convert.ToBase64String(md5.ComputeHash(Encoding.Default.GetBytes(text)));
+        }
+    }
+}
