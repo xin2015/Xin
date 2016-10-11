599c8666e2d55f91c4f81726e4c840e673a0b120
liuhx <564030525@qq.com>
2016-10-11 14:30:37 +0800

    division

--- Xin/Xin.ConsoleApplication/Program.cs
+++ Xin/Xin.ConsoleApplication/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Ivony.Html;
+using Ivony.Html.Parser;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Net;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +15,22 @@ namespace Xin.ConsoleApplication
     {
         static void Main(string[] args)
         {
-            string a = CryptogramHelper.EncryptByMD5("123456");
+            string url = "http://www.stats.gov.cn/tjsj/tjbz/xzqhdm/201608/t20160809_1386477.html";
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Code", typeof(int));
+            dt.Columns.Add("Name", typeof(string));
+            dt.TableName = "Division";
+            JumonyParser jp = new JumonyParser();
+            IHtmlDocument html = jp.LoadDocument(url, Encoding.UTF8);
+            List<IHtmlElement> list = html.Find(".MsoNormal").ToList();
+            foreach (IHtmlElement item in list)
+            {
+                DataRow dr = dt.NewRow();
+                dr["Code"] = int.Parse(item.Elements().First().InnerText().Trim());
+                dr["Name"] = item.Elements().Last().InnerText().Trim();
+                dt.Rows.Add(dr);
+            }
+            SqlHelper.Default.Insert(dt);
         }
     }
 }
