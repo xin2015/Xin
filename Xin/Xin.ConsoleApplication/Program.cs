2c02bc3ce5e450411bfe550d334bcd28e3415b55
LiuHaixin <liuhaixin2016@outlook.com>
2016-11-12 03:03:31 +0800

    PropertyAccessor

--- Xin/Xin.ConsoleApplication/Program.cs
+++ Xin/Xin.ConsoleApplication/Program.cs
@@ -4,6 +4,7 @@ using Ivony.Html.Parser;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Net;
@@ -19,17 +20,48 @@ namespace Xin.ConsoleApplication
     {
         static void Main(string[] args)
         {
-            Test a = new Test();
-            Type t = a.GetType();
-            t.GetProperty("Code").FastSetValue<Test>(a, "1001A");
-            t.GetProperty("Name").FastSetValueN(a, "监测中心站");
-            A b = new A();
-            b.Name = "AQI";
-            b.Value = 35;
-            t.GetProperty("Value").FastSetValueN(a, b);
-            object code = t.GetProperty("Code").FastGetValue<Test>(a);
-            object name = t.GetProperty("Name").FastGetValueN(a);
-            a = null;
+            Person a = new Person("liuhx", 25, "man");
+            Person b = new Person("liusp", 49, "man");
+            Person c = new Person("pangcf", 49, "woman");
+
+            int length = 1000000;
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            PropertyInfo property = a.GetType().GetProperty("DefaultName");
+            try
+            {
+                ParameterExpression instanceParam = Expression.Parameter(typeof(object));
+                MethodCallExpression call;
+                if (property.GetMethod.IsStatic)
+                {
+                    UnaryExpression instanceConvert = Expression.Convert(instanceParam, property.ReflectedType);
+                    call = Expression.Call(instanceConvert, property.GetMethod);
+                    var lambda = Expression.Lambda<Func<object, object>>(call, instanceParam);
+                    var func = lambda.Compile();
+                    string re = (string)func(a);
+                }
+                else
+                {
+                    UnaryExpression instanceConvert = Expression.Convert(instanceParam, property.ReflectedType);
+                    call = Expression.Call(instanceConvert, property.GetMethod);
+                    var lambda = Expression.Lambda<Func<object, object>>(call, instanceParam);
+                    var func = lambda.Compile();
+                    string re = (string)func(a);
+                }
+            }
+            catch (Exception e)
+            {
+                string me = e.Message;
+            }
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+            Console.ReadLine();
+
+
+
+
+
+
             //string url = "http://www.stats.gov.cn/tjsj/tjbz/xzqhdm/201608/t20160809_1386477.html";
             //DataTable dt = new DataTable();
             //dt.Columns.Add("Code", typeof(int));
@@ -55,11 +87,10 @@ namespace Xin.ConsoleApplication
         {
             MethodInfo method = property.GetSetMethod();
             var param_instance = Expression.Parameter(typeof(T), "instance");
-            var param_method = Expression.Parameter(typeof(MethodInfo), "method");
             var param_value = Expression.Parameter(typeof(object), "value");
             var body_value = Expression.Convert(param_value, property.PropertyType);
             var body = Expression.Call(param_instance, method, body_value);
-            Expression.Lambda<Action<T, MethodInfo, object>>(body, param_instance, param_method, param_value).Compile()(instance, method, value);
+            Expression.Lambda<Action<T, object>>(body, param_instance, param_value).Compile()(instance, value);
         }
 
         public static void FastSetValueN(this PropertyInfo property, object instance, object value)
@@ -92,19 +123,43 @@ namespace Xin.ConsoleApplication
             var body = Expression.Call(body_instance, method);
             return Expression.Lambda<Func<object, MethodInfo, object>>(body, param_instance, param_method).Compile()(instance, method);
         }
-
     }
 
-    public class Test
+    public class Person
     {
-        public string Code { get; set; }
-        public string Name { get; set; }
-        public A Value { get; set; }
-    }
+        public static string DefaultName { get; }
 
-    public class A
-    {
-        public string Name { get; set; }
-        public int Value { get; set; }
+        static Person()
+        {
+            DefaultName = "Default";
+        }
+
+        public string Name { get; }
+        public int Age { get; set; }
+        public string Sex { get; set; }
+        public List<Person> Family { get; set; }
+
+        public Person()
+        {
+            Family = new List<Person>();
+        }
+
+        public Person(string name, int age, string sex)
+        {
+            Name = name;
+            Age = age;
+            Sex = sex;
+            Family = new List<Person>();
+        }
+
+        public string Introduction()
+        {
+            return string.Format("Hello, my name is {0}, I'm {1} years old.", Name, Age);
+        }
+
+        public void AddFamily(Person p)
+        {
+            Family.Add(p);
+        }
     }
 }
---
+++ Xin/Xin/Expressions/PropertyAccessor.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xin.Expressions
+{
+    /// <summary>
+    /// Property（属性）快速反射接口
+    /// </summary>
+    public interface IPropertyAccessor
+    {
+        /// <summary>
+        /// GetValue
+        /// </summary>
+        /// <param name="instance">instance</param>
+        /// <returns></returns>
+        object GetValue(object instance);
+        /// <summary>
+        /// SetValue
+        /// </summary>
+        /// <param name="instance">instance</param>
+        /// <param name="value">value</param>
+        void SetValue(object instance, object value);
+        /// <summary>
+        /// Property
+        /// </summary>
+        PropertyInfo Property { get; }
+    }
+
+    /// <summary>
+    /// Property（属性）快速反射类
+    /// </summary>
+    public class PropertyAccessor : IPropertyAccessor
+    {
+        private Func<object, object> getter;
+        private Action<object, object> setter;
+
+        /// <summary>
+        /// Property
+        /// </summary>
+        public PropertyInfo Property { get; private set; }
+
+        /// <summary>
+        /// GetValue
+        /// </summary>
+        /// <param name="instance">instance</param>
+        /// <returns></returns>
+        public object GetValue(object instance)
+        {
+            if (getter == null)
+            {
+                throw new NotSupportedException("Get method is not defined for this property.");
+            }
+            return getter(instance);
+        }
+
+        /// <summary>
+        /// SetValue
+        /// </summary>
+        /// <param name="instance">instance</param>
+        /// <param name="value">value</param>
+        public void SetValue(object instance, object value)
+        {
+            if (setter == null)
+            {
+                throw new NotSupportedException("Set method is not defined for this property.");
+            }
+            setter(instance, value);
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="property">Property</param>
+        public PropertyAccessor(PropertyInfo property)
+        {
+            Property = property;
+            InitializeGet();
+            InitializeSet();
+        }
+
+        private void InitializeGet()
+        {
+            if (Property.CanRead)
+            {
+                ParameterExpression instanceParam = Expression.Parameter(typeof(object));
+                UnaryExpression instanceConvert = Property.GetMethod.IsStatic ? null : Expression.Convert(instanceParam, Property.ReflectedType);
+                MethodCallExpression call = Expression.Call(instanceConvert, Property.GetMethod);
+                getter = Expression.Lambda<Func<object, object>>(call, instanceParam).Compile();
+            }
+        }
+
+        private void InitializeSet()
+        {
+            if (Property.CanWrite)
+            {
+                ParameterExpression instanceParam = Expression.Parameter(typeof(object));
+                ParameterExpression valueParam = Expression.Parameter(typeof(object));
+                UnaryExpression instanceConvert = Property.SetMethod.IsStatic ? null : Expression.Convert(instanceParam, Property.ReflectedType);
+                UnaryExpression valueConvert = Expression.Convert(valueParam, Property.PropertyType);
+                MethodCallExpression call = Expression.Call(instanceConvert, Property.SetMethod, valueConvert);
+                setter = Expression.Lambda<Action<object, object>>(call, instanceParam, valueParam).Compile();
+            }
+        }
+    }
+}
