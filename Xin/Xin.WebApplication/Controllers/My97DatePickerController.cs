4fbc99bafe072237122ed1b0aaacc6870a81fabf
LiuHaixin <liuhaixin2016@outlook.com>
2017-07-04 17:07:15 +0800

    temp

---
+++ Xin/Xin.WebApplication/Controllers/My97DatePickerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Xin.WebApplication.Controllers
+{
+    public class My97DatePickerController : Controller
+    {
+        // GET: My97DatePicker
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Xin/Xin.WebApplication/Scripts/Common.js
+++ Xin/Xin.WebApplication/Scripts/Common.js
@@ -22,3 +22,62 @@ function GetTimeFromJsonString(jsonTime) {
     var d = new Date(parseInt(m[0]));
     return d;
 }
+
+Array.prototype.sum = function () {
+    var sum = this[0];
+    for (var i = 1; i < this.length; i++) {
+        sum += this[i];
+    }
+    return sum;
+}
+
+Array.prototype.max = function () {
+    var max = this[0];
+    for (var i = 1; i < this.length; i++) {
+        if (this[i] > max) {
+            max = this[i];
+        }
+    }
+    return max;
+}
+
+Array.prototype.min = function () {
+    var min = this[0];
+    for (var i = 1; i < this.length; i++) {
+        if (this[i] < min) {
+            min = this[i];
+        }
+    }
+    return min;
+}
+
+Array.prototype.avg = function () {
+    if (this.length === 0) return undefined;
+    else return this.sum() / this.length;
+}
+
+function CorrelationCoefficient(arrayA, arrayB) {
+    var avgA = arrayA.avg();
+    var avgB = arrayB.avg();
+    var cov = 0;
+    var varA = 0;
+    var varB = 0;
+    for (var i = 0; i < arrayA.length; i++) {
+        cov += (arrayA[i] - avgA) * (arrayB[i] - avgB);
+        varA += Math.pow((arrayA[i] - avgA), 2);
+        varB += Math.pow((arrayB[i] - avgB), 2);
+    }
+    return cov / Math.sqrt(varA * varB);
+}
+
+function CorrelationCoefficientAssessment(correlationCoefficient) {
+    var result;
+    correlationCoefficient = Math.abs(correlationCoefficient);
+    if (correlationCoefficient === 0) result = "不存在线性相关";
+    else if (correlationCoefficient < 0.3) result = "为极弱线性相关";
+    else if (correlationCoefficient < 0.5) result = "为低度线性相关";
+    else if (correlationCoefficient < 0.8) result = "为中度线性相关";
+    else if (correlationCoefficient < 1) result = "为高度线性相关";
+    else result = "为完全线性相关";
+    return result;
+}
